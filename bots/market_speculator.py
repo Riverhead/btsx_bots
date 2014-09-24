38b88c34a2fc59a14bdc17fe08496f2969c8972f
Riverhead <james.reidy@gmail.com>
2014-09-24 10:07:21 -0400

    Simplified logic to be based on lowest_ask

--- bots/market_speculator.py
+++ bots/market_speculator.py
@@ -1,15 +1,8 @@
-##  This is a very simple bot based on the Market Maker bot for BitSharesX. This takes logic started by "toast" and massaged by Riverhead.
+##  This is a very simple bot based on the Market Maker bot for BitSharesX. This takes logic started by toast and massaged by Riverhead.
 ##  
 ##  IT IS HIGHLY RECOMMENDED YOU SETUP A BOT WALLET/ACCOUNT WITH LIMITED FUNDS. IF YOU RUN THIS AGAINST YOUR PRIMARY WALLET REALLY "BAD THINGS" COULD HAPPEN.
 ##  
 ##  The purpose of this bot is: 1) Create a simple bot as a base to create more complex bots 2) Generate traffic in the bitUSD:BTSX market
-##  
-##  SETUP:
-##  
-##  Modify the config.json to reference your own RPC configutarion.
-##  Fund your bot account with at least 501 BTSX
-##  create a logs directory in the same directory as the python source
-##  
 
 import time
 
