7bc900a36fa79237b59b81eddc5404c7f07db486
Riverhead <james.reidy@gmail.com>
2014-09-26 07:46:12 -0400

    Bot is now asset agnostic

--- bots/market_speculator.py
+++ bots/market_speculator.py
@@ -1,8 +1,15 @@
-##  This is a very simple bot based on the Market Maker bot for BitSharesX. This takes logic started by toast and massaged by Riverhead.
+##  This is a very simple asset agnostic bot based on the Market Maker bot for BitSharesX. This takes logic started by "toast" and massaged by Riverhead.
 ##  
 ##  IT IS HIGHLY RECOMMENDED YOU SETUP A BOT WALLET/ACCOUNT WITH LIMITED FUNDS. IF YOU RUN THIS AGAINST YOUR PRIMARY WALLET REALLY "BAD THINGS" COULD HAPPEN.
 ##  
-##  The purpose of this bot is: 1) Create a simple bot as a base to create more complex bots 2) Generate traffic in the bitUSD:BTSX market
+##  The purpose of this bot is: 1) Create a simple bot as a base to create more complex bots 2) Generate traffic in the bitAsset:BTSX market
+##  
+##  SETUP:
+##  
+##  Modify the config.json to reference your own RPC configutarion.
+##  Fund your bot account with at least 501 BTSX
+##  create a logs directory in the same directory as the python source
+##  
 
 import time
 
@@ -16,15 +23,23 @@ class MarketSpeculator():
         self.name = self.config["account_name"]
         self.quote_symbol = bot_config["asset_pair"][0]
         self.base_symbol = bot_config["asset_pair"][1]
-        self.min_balance = self.config["min_balance"]
+        self.min_base_balance = self.config["min_base_balance"]
+        self.min_quote_balance = self.config["min_quote_balance"]
+        self.spread = self.config["spread_percent"]
+        self.beat_bid_by = self.config["beat_bid_by"]
+        self.beat_ask_by = self.config["beat_ask_by"]
+
         self.last_bid = 0
         self.last_ask = 0
 
     def execute(self):
         self.log("Executing bot:  %s" % self.name)
-        SPREAD = self.config["spread_percent"]
-        BEAT_BUY_BY = 0.005
-        BEAT_ASK_BY = 0.005
+        SPREAD = self.spread
+        BEAT_BID_BY = self.beat_bid_by  #Requires get_lowest_bid() which isn't done yet
+        BEAT_ASK_BY = self.beat_ask_by
+
+        base_precision  = self.client.get_precision(self.base_symbol)
+        quote_precision = self.client.get_precision(self.quote_symbol)
 
         median = self.client.get_median(self.quote_symbol)
 
@@ -33,31 +48,36 @@ class MarketSpeculator():
 
         #Get the ratio of the lowest ask price
         lowest_ask  = self.client.get_lowest_ask(self.quote_symbol, self.base_symbol)
+        lowest_ask  = lowest_ask * (base_precision / quote_precision)
 
-        usd_balance = self.client.get_balance(self.name, self.quote_symbol) 
-        btsx_balance = self.client.get_balance(self.name, self.base_symbol) 
+        quote_balance = self.client.get_balance(self.name, self.quote_symbol) 
+        base_balance = self.client.get_balance(self.name, self.base_symbol) 
 
         #If the market has moved.
-        if ((abs(self.last_ask - last_price) / last_price) > (SPREAD / 3)) and self.last_ask != 0 :
-           self.client.cancel_all_orders(self.name, self.base_symbol, self.quote_symbol)
-           self.client.wait_for_block() 
+        #if ((abs(self.last_ask - last_price) / last_price) > (SPREAD / 3)) and self.last_ask != 0 :
+           #self.client.cancel_all_orders(self.name, self.base_symbol, self.quote_symbol)
+           #self.client.wait_for_block() 
 
-        if ((abs(self.last_bid - last_price) / last_price) > (SPREAD / 3)) and self.last_bid != 0 :
-           self.client.cancel_all_orders(self.name, self.base_symbol, self.quote_symbol)
-           self.client.wait_for_block() 
+        #if ((abs(self.last_bid - last_price) / last_price) > (SPREAD / 3)) and self.last_bid != 0 :
+           #self.client.cancel_all_orders(self.name, self.base_symbol, self.quote_symbol)
+           #self.client.wait_for_block() 
 
         #bid just below the lowest ask 
 
-        if usd_balance > 10:
-           self.client.submit_bid(self.name, ((usd_balance-1) / (lowest_ask *(1-(BEAT_ASK_BY*2)))), self.base_symbol, lowest_ask * (1-(BEAT_ASK_BY*2)) , self.quote_symbol)
+
+        if quote_balance > self.min_quote_balance:
+           self.log ("submitting bid for %f" % (lowest_ask * (1-(BEAT_ASK_BY*2))))
+           self.client.submit_bid(self.name, ((quote_balance-min_quote_balance) / (lowest_ask *(1-(BEAT_ASK_BY*2)))), self.base_symbol, lowest_ask * (1-(BEAT_ASK_BY*2)) , self.quote_symbol)
            self.last_bid = lowest_ask * (1-BEAT_ASK_BY*2)
            self.last_ask = 0
 
 
         #For selling we just want to sell a few Larimers less to make sure we're the cheapest
 
-        if btsx_balance > 50:
-           self.client.submit_ask(self.name, btsx_balance-(self.min_balance-50), self.base_symbol, lowest_ask * (1-BEAT_ASK_BY), self.quote_symbol)
+        if base_balance > self.min_base_balance:
+           self.log ("submitting ask for %f" %  (lowest_ask * (1-(BEAT_ASK_BY))))
+           self.client.submit_ask(self.name, base_balance-self.min_base_balance, self.base_symbol, lowest_ask * (1-BEAT_ASK_BY), self.quote_symbol)
+           self.last_ask = lowest_ask * (1-BEAT_ASK_BY)
            self.last_bid = 0
 
 
